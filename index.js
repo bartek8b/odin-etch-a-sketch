54f1781227c5e2c4780298d05b71a3b55a176b02
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-03-26 22:15:58 +0100

    Add create grid logic, adjustable cells

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const container = document.querySelector("#container");
 
-
 function createGrid(limit){
     if(limit > 100 || limit < 16) {
         alert("Choose the number in range 16-100");
     }
     else{
         for(let i = 1; i <= limit; i++){
-            container.style.width = `${20 * limit}px`;
+
             const row = document.createElement("div");
+            row.style.height = `${(window.innerHeight * 0.7) / limit}px`
             container.appendChild(row);
             row.classList.add("rows");
 
             for(let i = 1; i <= limit; i++){
                 const cell = document.createElement("div");
+                cell.style.width = `${(window.innerHeight * 0.7) / limit}px`;
                 row.appendChild(cell);
                 cell.classList.add("cells");
             }
@@ -21,4 +22,4 @@ function createGrid(limit){
     }
 }
 
-createGrid(16);
+createGrid(25);
