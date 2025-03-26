c904e00c968fc62399422b3cc123d1bad6c0f53c
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-03-26 19:51:06 +0100

    Add cells adding logic

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ function createGrid(limit){
     if(limit > 100 || limit < 16) {
         alert("Choose the number in range 16-100");
     }
-    else{       
-
-        for(let i = 1; i < limit; i++){
+    else{
+        for(let i = 1; i <= limit; i++){
+            container.style.width = `${20 * limit}px`;
             const row = document.createElement("div");
             container.appendChild(row);
+            row.classList.add("rows");
+
+            for(let i = 1; i <= limit; i++){
+                const cell = document.createElement("div");
+                row.appendChild(cell);
+                cell.classList.add("cells");
+            }
         }
     }
 }
